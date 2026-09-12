0e780405dcb60a2557a08764aea683c60571fb78
Kutter Edge <kutteredgestudios@gmail.com>
2026-09-12 17:18:11 -0400

    Update psk.php

--- psk.php
+++ psk.php
@@ -1,10 +1,7 @@
 <?php
 header("Content-Type: application/json");
 
-// PSKReporter query: VHF only (50/144/222/432 MHz)
 $url = "https://pskreporter.info/cgi-bin/pskquery?band=VHF&mode=ALL&hours=1&format=json";
 
-$data = file_get_contents($url);
-
-echo $data;
+echo file_get_contents($url);
 ?>
